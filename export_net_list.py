62aac4f2ad3bd295375418769607c508cbaf7e33
Ethan Chien <liangtie.qian@gmail.com>
2024-04-24 10:24:51 +0800

    add export net list

---
+++ export_net_list.py
@@ -0,0 +1,89 @@
+import os
+import subprocess
+import uuid
+import logging
+
+kicad_img_id = "8b2c01c7dc6e"
+kicad_img_home_path ="/home/kicad"
+
+'''
+        String[] firstCmd = { "docker", "run", "-v", kicad_project_dir + ":" + mounted_prj_path,
+                kicad_img_id, "kicad-cli", "sch",
+                "export", "netlist", "--format", "allegro",
+                mouted_sch_root_path, "-o",
+                mounted_prj_path + "/" + output_file_name
+        };
+
+
+'''
+#
+def export_glb(root_sch_file_name):
+    kicad_project_dir = os.path.dirname(root_sch_file_name)
+    pcb_name = os.path.basename(root_sch_file_name)
+    mounted_prj_path = os.path.join(kicad_img_home_path, str(uuid.uuid4())).replace("\\", "/")
+    mouted_pcb_fp = os.path.join(mounted_prj_path, pcb_name).replace("\\", "/")
+    output_file_name = str(uuid.uuid4()) + ".txt"
+    # docker_output_fn = os.path.join(mounted_prj_path, output_file_name).replace("\\", "/")
+
+    first_cmd = [
+        "docker", "run",
+        "-v", f"{kicad_project_dir}:{mounted_prj_path}",
+        kicad_img_id, 
+        "kicad-cli", "sch",
+        "export", "netlist", "--format", "allegro",
+        mouted_pcb_fp, "-o",
+        f"{mounted_prj_path}/{output_file_name}"
+    ]
+
+    print(" ".join(first_cmd))
+
+    try:
+        process_export = subprocess.Popen(first_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        _, stderr = process_export.communicate()
+        if stderr:
+            logging.error(stderr.decode())
+    except Exception as e:
+        logging.error(e)
+
+    try:
+        process_export.wait()
+    except Exception as e:
+        logging.error(e)
+
+    # c_output_file_name = str(uuid.uuid4()) + ".glb"
+    # c_docker_output_fn = os.path.join(mounted_prj_path, c_output_file_name).replace("\\", "/")
+
+    # second_cmd = ["docker", "run", "-v", f"{kicad_project_dir}:{mounted_prj_path}",
+    #               kicad_img_id, "npx", "gltfpack",
+    #               "-i",
+    #               docker_output_fn, "-v", "-cc", "-tc", "-ts", "0.5", "-o",
+    #               c_docker_output_fn
+    #               ]
+
+    # print(" ".join(second_cmd))
+
+    # try:
+    #     process_pack = subprocess.Popen(second_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #     _, stderr = process_pack.communicate()
+    #     if stderr:
+    #         logging.error(stderr.decode())
+    # except Exception as e:
+    #     logging.error(e)
+
+    # try:
+    #     process_pack.wait()
+    # except Exception as e:
+    #     logging.error(e)
+
+    # return os.path.join(kicad_project_dir, c_output_file_name).replace("\\", "/")
+
+
+def main():
+    import time
+
+    export_glb(r"D:/code/kicad-cli-python/video.kicad_sch")
+
+
+
+if __name__ == "__main__":
+    main()
